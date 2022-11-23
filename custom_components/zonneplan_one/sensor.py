cc2e137d5559326f75bf712983cadebf4dc344c9
Frans Saris <franssaris@gmail.com>
2022-11-23 19:29:14 +0100

    Fixed skip_update_based_on_daily_update_hour

--- custom_components/zonneplan_one/sensor.py
+++ custom_components/zonneplan_one/sensor.py
@@ -2,6 +2,7 @@
 from typing import Optional
 
 from voluptuous.validators import Number
+
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
 )
@@ -163,17 +164,41 @@ class ZonneplanSensor(CoordinatorEntity, SensorEntity):
         if value is None and self.entity_description.none_value_behaviour == NONE_USE_PREVIOUS:
             return
 
-        if self.entity_description.daily_update_hour is not None and (state := self.hass.states.get(self.entity_id)):
-            update_today = dt_util.now().replace(hour=self.entity_description.daily_update_hour, minute=0, second=0, microsecond=0)
-            _LOGGER.debug(f'Last update {self.name}: {dt_util.as_local(state.last_updated)} < {update_today}')
-            if update_today < dt_util.now() or (state.last_updated and dt_util.as_local(state.last_updated) >= update_today):
-                _LOGGER.info(f'Skip update {self.name} until {self.entity_description.daily_update_hour}h')
-                return
+        if self.skip_update_based_on_daily_update_hour():
+            _LOGGER.info(f'Skip update {self.name} until {self.entity_description.daily_update_hour}h')
+            return
 
         _LOGGER.debug(f'Update {self.name}: {value}')
 
         self._attr_native_value = value
         self.async_write_ha_state()
+
+    def skip_update_based_on_daily_update_hour(self) -> bool:
+
+        if self.entity_description.daily_update_hour is None:
+            return False
+
+        # No state? then we update
+        if not (state := self.hass.states.get(self.entity_id)):
+            return False
+
+        # No last update value? then we update
+        if not state.last_updated:
+            return False
+
+        update_today = dt_util.now().replace(hour=self.entity_description.daily_update_hour, minute=0, second=0, microsecond=0)
+
+        # Is it time already to update the value today? No then we skip
+        if update_today > dt_util.now():
+            _LOGGER.debug(f'Skipped update {self.name}: {update_today} (update today) > {dt_util.now()} (now)')
+            return True
+
+        # Already updated today after daily_update_hour? Then skip
+        if dt_util.as_local(state.last_updated) >= update_today:
+            _LOGGER.debug(f'Skipped update {self.name}: {dt_util.as_local(state.last_updated)} (last update) >= {update_today} (update today)')
+            return True
+
+        return False
 
     def _value_from_coordinator(self):
         raw_value = value = self.coordinator.getConnectionValue(
