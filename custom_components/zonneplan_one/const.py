0ebbca8c48e6ff79341bcf558a56a028b953a814
Frans Saris <franssaris@gmail.com>
2024-10-15 21:43:40 +0200

    Bump version to 2024.10.1

--- custom_components/zonneplan_one/const.py
+++ custom_components/zonneplan_one/const.py
@@ -34,7 +34,7 @@ BATTERY = "home_battery_installation"
 NONE_IS_ZERO = "none-is-zero"
 NONE_USE_PREVIOUS = "none-is-previous"
 
-VERSION = "2024.10.0"
+VERSION = "2024.10.1"
 
 @dataclass
 class Attribute:
